336e5cc2c6a568acfb6b736deabb06073d618a3d
Rommel Rico <me@rommelrico.com>
2019-01-03 06:53:21 -0800

    Using a named export

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 // Action Creator
-const selectSong = (song) => {
+export const selectSong = (song) => {
     // Return an action
     return {
         type: 'SONG_SELECTED',
         payload: song
     };
 };
+
