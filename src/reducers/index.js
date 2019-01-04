2d71a04704d960a5459b5684169151436d9fbe49
Rommel Rico <me@rommelrico.com>
2019-01-04 07:01:26 -0800

    Initial selectedSongReducer implementation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,3 +6,11 @@ const songsReducer = () => {
         { title: 'I want it that way', duration: '3:13' }
     ];
 };
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
