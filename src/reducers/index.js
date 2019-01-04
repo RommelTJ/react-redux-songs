30ef42d660fd6da92dcafa1e6ce694e7b61a1ea4
Rommel Rico <me@rommelrico.com>
2019-01-04 06:59:22 -0800

    Initial songsReducer implementation

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        { title: 'Smells Like Teen Spirit', duration: '4:05' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'All Star', duration: '3:23' },
+        { title: 'I want it that way', duration: '3:13' }
+    ];
+};
